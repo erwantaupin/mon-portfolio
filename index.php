0febd669b99ce57efec9166eb0ac373fd6026670
erwantaupin <taupin.erwan@laposte.net>
2022-02-10 15:31:28 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>my-portfolio</title>
+</head>
+<body>
+    <header>
+        <div class="box-menu">
+            <div class="menu">
+                <div class="Profil"><img src="" alt="profil"></div>
+                <div class="Accueil"></div>
+                <div class="Competence"><a href="#"></a></div>
+                <div class="Projet"></div>
+                <div class="Formulaire"><a href="#"></a></div>
+            </div>
+        </div>
+    </header>
+    <div class="paral">
+
+    </div>
+    <div class="histoire">
+        <div class="vecu">
+
+        </div>
+        <div class="comp">
+
+        </div>
+    </div>
+    <div class="proje">
+        <div class="sliderpro">
+
+        </div>
+    </div>
+    <div class="formulaire">
+
+    </div>
+    <div class="footer">
+        
+    </div>
+</body>
+</html>
