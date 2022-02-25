2530700080f48ed7ed22466204c9a35ef14dcd27
erwantaupin <taupin.erwan@laposte.net>
2022-02-25 16:12:51 +0100

    modif

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,19 +1,28 @@
 // burger
-var btn = document.querySelector('.toggle_btn');
+
+
+
+if (window.matchMedia("(max-width: 1024px)").matches) {
+    var btn = document.querySelector('.toggle_btn');
 var nav = document.querySelector('.nav');
 var ouvert = false;
 var blur = document.getElementById("blur");
 
 btn.onclick = function(){
     nav.classList.toggle('nav_open');
-    if (ouvert) {
-        ouvert = false;
-        blur.style.filter= "blur(0px)";
-    }
-    else {
-        ouvert = true;
-        blur.style.filter= "blur(4px)";
-    } 
+        if (ouvert) {
+                    ouvert = false;
+                    blur.style.filter= "blur(0px)";
+                    }
+        else {
+            ouvert = true;
+            blur.style.filter= "blur(4px)";
+            }
 }
+  } 
+
+else {}
+  
+
 
 
