4560a643eaf1df78f3bf43a7c893ca9d5ea4af00
erwantaupin <taupin.erwan@laposte.net>
2022-02-28 09:19:57 +0100

    essai limit btn

--- assets/js/btn.js
+++ assets/js/btn.js
@@ -1,4 +1,14 @@
 const btn = document.querySelector(".btn");
+// a = 600;
+
+// window.addEventListener("scroll", () => {
+//   if (scrollY == a) {
+//     function changeStyle() {
+//       document.getElementsByClassName("btn")[0].style.display = "flex";
+//     }
+//   }
+//   console.log(window.scrollY);
+// });
 
 btn.addEventListener("click", () => {
   window.scrollTo({
